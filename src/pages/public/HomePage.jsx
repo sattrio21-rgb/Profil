c732b6797c86365f73894441909e75346dc97207
brewiratama12-coder <sattrio21@student.ub.ac.id>
2026-06-29 22:02:25 +0700

    Home Page

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -80,7 +80,7 @@ const HomePage = () => {
     <div className="min-h-screen w-5xl mx-auto bg-[#0a0a0a] text-white">
       <HeroSection profile={profile} />
       <PendidikanSection pendidikans={pendidikans} />
-      <PengalamanSection />
+      <PenglamanSection />
       <ProjectSection projects={projects} />
     </div>
   )
